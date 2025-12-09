99f1e8d3f32b934a26ef2f431c953c14e2b96601
MasterB3ar <bailiff.talking9a@icloud.com>
2025-12-09 09:06:58 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,59 +5,44 @@
     # NixOS 24.05 channel
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
-    # Impermanence (we can use later for persistence, but safe to keep here)
+    # Impermanence (optionally used later for persistence)
     impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = { self, nixpkgs, impermanence, ... }:
-  let
-    system = "x86_64-linux";
-    lib = nixpkgs.lib;
-  in
-  {
-    ########################################
-    ## NixOS configuration for the ISO
-    ########################################
-    nixosConfigurations.chernos-iso = lib.nixosSystem {
-      inherit system;
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+    in
+    {
+      ########################################
+      ## NixOS configuration for the ISO
+      ########################################
+      nixosConfigurations = {
+        chernos-iso = lib.nixosSystem {
+          inherit system;
 
-      # If you want to pass flake inputs into modules later:
-      specialArgs = {
-        inherit impermanence;
+          specialArgs = {
+            inherit impermanence;
+          };
+
+          modules = [
+            ./hosts/chernos-iso/configuration.nix
+          ];
+        };
       };
 
-      modules = [
-        ./hosts/chernos-iso/configuration.nix
-      ];
+      ########################################
+      ## Expose ISO as a flake package
+      ########################################
+      # You can:
+      #   nix build .#iso
+      # or:
+      #   nix build .#packages.x86_64-linux.iso
+      packages.${system}.iso =
+        self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
+
+      # Optional: make ISO the default package
+      defaultPackage.${system} = self.packages.${system}.iso;
     };
-
-    ########################################
-    ## Expose ISO as a flake package
-    ########################################
-    # So you can:
-    #   nix build .#iso
-    # or:
-    #   nix build .#packages.x86_64-linux.iso
-    packages.${system}.iso =
-      self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
-
-    # Optional: make ISO the default package
-    defaultPackage.${system} = self.packages.${system}.iso;
-
-    ########################################
-    ## (Optional) devShell for the UI project
-    ########################################
-    devShells.${system}.default = let
-      pkgs = import nixpkgs { inherit system; };
-    in pkgs.mkShell {
-      buildInputs = with pkgs; [
-        nodejs_22
-        pnpm
-        git
-      ];
-      shellHook = ''
-        echo "ChernOS UI dev shell – run 'cd ui && pnpm install && pnpm dev'"
-      '';
-    };
-  }
 }
