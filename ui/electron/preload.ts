d88a566da2cd985ec677490b6747bb449fb0635d
MasterB3ar <bailiff.talking9a@icloud.com>
2025-12-09 13:27:42 +0100

    Delete ui/electron directory

--- ui/electron/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("chernosAPI", {
-  onHotkey(callback: (payload: any) => void) {
-    ipcRenderer.on("chernos/hotkey", (_event, payload) => callback(payload));
-  },
-  // For future: persist, logs, etc.
-});
