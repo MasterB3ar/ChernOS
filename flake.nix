b6eb8dd438a412396c2a4230990d91249754a0f8
MasterB3ar <bailiff.talking9a@icloud.com>
2025-12-09 13:28:37 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,38 @@
 {
-  description = "ChernOS 2.0 – XFCE live ISO";
+  description = "ChernOS v2.0.0 – Reactor Shell ISO";
 
   inputs = {
-    # Stable NixOS 24.05
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    # You can keep impermanence if you want later, but it is not used right now.
     impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = { self, nixpkgs, impermanence, ... }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      # Main NixOS configuration for the ISO
-      nixosConfigurations.chernos-iso = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./hosts/chernos-iso/configuration.nix
-        ];
-      };
-
-      # Convenience attribute: nix build .#packages.x86_64-linux.iso
-      packages.${system}.iso =
-        self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
-
-      # Default package when you just do: nix build
-      defaultPackage.${system} = self.packages.${system}.iso;
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true; # chromium
     };
+  in {
+    nixosConfigurations.chernos-iso = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ({ modulesPath, ... }: {
+          imports = [
+            # NixOS official minimal ISO base
+            (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+            impermanence.nixosModules.impermanence
+            ./hosts/chernos-iso/configuration.nix
+          ];
+        })
+      ];
+    };
+
+    # Convenience: nix build .#packages.x86_64-linux.iso
+    packages.${system}.iso =
+      self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
+
+    # nix build
+    defaultPackage.${system} = self.packages.${system}.iso;
+  };
 }
