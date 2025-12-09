6a61a882a1fdf8c29b77fb35854f9293f033ff90
MasterB3ar <bailiff.talking9a@icloud.com>
2025-12-09 11:21:56 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,52 +1,32 @@
 {
-  description = "ChernOS 2.0 – NixOS ISO + UI";
+  description = "ChernOS 2.0 – XFCE live ISO";
 
   inputs = {
-    # NixOS 24.05 channel (stable base)
+    # Stable NixOS 24.05
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
-    # Impermanence (optional, for persistence later)
+    # You can keep impermanence if you want later, but it is not used right now.
     impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = { self, nixpkgs, impermanence, ... }:
     let
       system = "x86_64-linux";
-      lib = nixpkgs.lib;
     in
     {
-      ########################################
-      ## NixOS configuration for the ISO
-      ########################################
-      nixosConfigurations = {
-        chernos-iso = lib.nixosSystem {
-          inherit system;
-
-          specialArgs = {
-            inherit impermanence;
-          };
-
-          modules = [
-            # 1) Standard NixOS graphical ISO with Calamares installer
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares.nix"
-
-            # 2) Your custom ChernOS ISO config
-            ./hosts/chernos-iso/configuration.nix
-          ];
-        };
+      # Main NixOS configuration for the ISO
+      nixosConfigurations.chernos-iso = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/chernos-iso/configuration.nix
+        ];
       };
 
-      ########################################
-      ## Expose ISO as a flake package
-      ########################################
-      # Build with:
-      #   nix build .#iso
-      # or:
-      #   nix build .#packages.x86_64-linux.iso
+      # Convenience attribute: nix build .#packages.x86_64-linux.iso
       packages.${system}.iso =
         self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
 
-      # Optional: makes `nix build .` build the ISO by default
+      # Default package when you just do: nix build
       defaultPackage.${system} = self.packages.${system}.iso;
     };
 }
