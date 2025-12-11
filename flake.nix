910ebaac5f5fd818e815bc74abd34ecc2072b08b
MasterB3ar <bailiff.talking9a@icloud.com>
2025-12-11 09:50:50 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,31 +1,52 @@
 {
-  description = "ChernOS v2.0.0 – reactor control ISO";
+  description = "ChernOS v2.0.0 – Reactor Shell ISO";
 
   inputs = {
+    # NixOS 24.05 release
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    # Persistence module
+    impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, impermanence, ... }:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
-    # NixOS ISO configuration
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    ############################################################################
+    # NixOS configuration: chernos-iso
+    ############################################################################
+
     nixosConfigurations.chernos-iso = nixpkgs.lib.nixosSystem {
       inherit system;
+
       modules = [
+        # ISO image module (gives us config.system.build.isoImage)
+        ({ modulesPath, ... }: {
+          imports = [
+            (modulesPath + "/installer/cd-dvd/iso-image.nix")
+          ];
+        })
+
+        # Impermanence: provides environment.persistence.*
+        impermanence.nixosModules.impermanence
+
+        # ChernOS system config
         ./hosts/chernos-iso/configuration.nix
       ];
     };
+
+    ############################################################################
+    # Packages / default
+    ############################################################################
 
     # Convenience: nix build .#packages.x86_64-linux.iso
     packages.${system}.iso =
       self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
 
-    # Convenience default: nix build
+    # So `nix build` (without attr) also builds the ISO
     defaultPackage.${system} = self.packages.${system}.iso;
   };
 }
