ba1aa46af10edf0fbc0aa7fab9993e42a7ec5b72
MasterB3ar <bailiff.talking9a@icloud.com>
2025-12-16 15:24:52 +0100

    Enhance flake.nix with nixConfig and clean comments
    Added nixConfig for experimental features and updated comments for clarity.

--- flake.nix
+++ flake.nix
@@ -2,51 +2,50 @@
   description = "ChernOS v2.0.0 – Reactor Shell ISO";
 
   inputs = {
-    # NixOS 24.05 release
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    # Persistence module
     impermanence.url = "github:nix-community/impermanence";
+  };
+
+  # Helps on GitHub runners and on machines where flakes aren't enabled globally
+  nixConfig = {
+    extra-experimental-features = "nix-command flakes";
   };
 
   outputs = { self, nixpkgs, impermanence, ... }:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
   in
   {
-    ############################################################################
-    # NixOS configuration: chernos-iso
-    ############################################################################
-
     nixosConfigurations.chernos-iso = nixpkgs.lib.nixosSystem {
       inherit system;
 
       modules = [
-        # ISO image module (gives us config.system.build.isoImage)
-        ({ modulesPath, ... }: {
+        # Base ISO module
+        ({ ... }: {
           imports = [
-            (modulesPath + "/installer/cd-dvd/iso-image.nix")
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
           ];
+
+          # ISO identity
+          isoImage.isoName = "ChernOS-v2.0.0-x86_64-linux.iso";
+          isoImage.volumeID = "CHERNOS_2_0_0";
+
+          # Keep ISO leaner
+          documentation.enable = false;
         })
 
-        # Impermanence: provides environment.persistence.*
+        # Impermanence provides environment.persistence.*
         impermanence.nixosModules.impermanence
 
-        # ChernOS system config
+        # Your actual OS config
         ./hosts/chernos-iso/configuration.nix
       ];
     };
 
-    ############################################################################
-    # Packages / default
-    ############################################################################
-
-    # Convenience: nix build .#packages.x86_64-linux.iso
+    # Convenience build: nix build .#iso
     packages.${system}.iso =
       self.nixosConfigurations.chernos-iso.config.system.build.isoImage;
 
-    # So `nix build` (without attr) also builds the ISO
     defaultPackage.${system} = self.packages.${system}.iso;
   };
 }
