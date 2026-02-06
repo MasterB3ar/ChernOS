578d64b937f31a5ce86dc29b1814d68813e016fb
MasterB3ar <bailiff.talking9a@icloud.com>
2026-02-06 16:07:17 +0100

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -2,17 +2,68 @@
 
 const { contextBridge, ipcRenderer } = require('electron');
 
-function safeStr(x) {
-  try { return String(x); } catch (_) { return ''; }
-}
-
+// Expose a small, safe bridge. The UI detects presence via `window.chernosDesktop`.
 contextBridge.exposeInMainWorld('chernosDesktop', {
-  appInfo: () => ipcRenderer.invoke('chernos:getAppInfo'),
-  showAbout: () => ipcRenderer.invoke('chernos:showAbout'),
-  openExternal: (url) => ipcRenderer.invoke('chernos:openExternal', safeStr(url)),
-  openAppWindow: (appId, options) => ipcRenderer.invoke('chernos:openAppWindow', safeStr(appId), options || {}),
-  toggleFullscreen: () => ipcRenderer.invoke('chernos:toggleFullscreen'),
-  setFullscreen: (enabled) => ipcRenderer.invoke('chernos:setFullscreen', !!enabled),
-  toggleKiosk: () => ipcRenderer.invoke('chernos:toggleKiosk'),
-  setKiosk: (enabled) => ipcRenderer.invoke('chernos:setKiosk', !!enabled)
+  isDesktop: true,
+
+  // Open an app window. `opts` supports { newInstance?: boolean, instance?: number }.
+  openAppWindow: (appId, opts = {}) => {
+    try {
+      ipcRenderer.send('chernos:openAppWindow', { appId, opts });
+      return true;
+    } catch (_) {
+      return false;
+    }
+  },
+
+  // List all windows known to the desktop suite.
+  listWindows: () => ipcRenderer.invoke('chernos:listWindows'),
+
+  // Focus a specific window by key.
+  focusWindow: (key) => {
+    try {
+      ipcRenderer.send('chernos:focusWindow', key);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  },
+
+  // Close a specific window by key.
+  closeWindow: (key) => {
+    try {
+      ipcRenderer.send('chernos:closeWindow', key);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  },
+
+  // Focus the main window.
+  focusMainWindow: () => {
+    try {
+      ipcRenderer.send('chernos:focusMainWindow');
+      return true;
+    } catch (_) {
+      return false;
+    }
+  },
+
+  showAbout: () => {
+    try {
+      ipcRenderer.send('chernos:showAbout');
+      return true;
+    } catch (_) {
+      return false;
+    }
+  },
+
+  openExternal: (url) => {
+    try {
+      ipcRenderer.send('chernos:openExternal', url);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  },
 });
